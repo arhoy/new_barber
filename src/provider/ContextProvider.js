521554f4df90fab5b0818d60f6997da2a642544d
arhoy <you@example.com>
2020-01-27 02:41:00 -0700

    debugging

--- src/provider/ContextProvider.js
+++ src/provider/ContextProvider.js
@@ -21,6 +21,7 @@ const ContextProvider = ({ children }) => {
 
   useEffect(() => {
     const initializeCheckout = async () => {
+      console.log('THIS IS THE SHOPNAME', process.env.SHOP_NAME);
       // Check for an existing cart.
       const isBrowser = typeof window !== 'undefined';
       const existingCheckoutID = isBrowser
