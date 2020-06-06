79379928c6770dc682befb2779a083e7f358e680
arhoy <you@example.com>
2020-06-06 16:28:17 -0600

    removed insta node

--- src/components/Insta.js
+++ src/components/Insta.js
@@ -1,56 +1,56 @@
-import React from 'react';
-import useInsta from '../hooks/useInsta';
-import Image from 'gatsby-image';
-import styled from '@emotion/styled';
-import { H2 } from './reusableStyles/typography/Typography';
+// import React from 'react';
+// import useInsta from '../hooks/useInsta';
+// import Image from 'gatsby-image';
+// import styled from '@emotion/styled';
+// import { H2 } from './reusableStyles/typography/Typography';
 
-const Container = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  margin: 1rem -0.5rem;
-  padding: 0.5rem 0;
-`;
+// const Container = styled.div`
+//   display: flex;
+//   flex-wrap: wrap;
+//   margin: 1rem -0.5rem;
+//   padding: 0.5rem 0;
+// `;
 
-const A = styled.a`
-  width: 120px;
-  box-shadow: 0;
-  display: block;
-  margin: 0.5rem;
-  max-width: calc(33% - 1rem);
-  width: 120px;
-  transition: 200ms box-shadow linear;
-  :focus,
-  :hover {
-    box-shadow: 0 2px 14px #22222244;
-    z-index: 2;
-  }
-`;
+// const A = styled.a`
+//   width: 120px;
+//   box-shadow: 0;
+//   display: block;
+//   margin: 0.5rem;
+//   max-width: calc(33% - 1rem);
+//   width: 120px;
+//   transition: 200ms box-shadow linear;
+//   :focus,
+//   :hover {
+//     box-shadow: 0 2px 14px #22222244;
+//     z-index: 2;
+//   }
+// `;
 
-const Insta = () => {
-  const instagramData = useInsta({ variables: { limit: 1 } });
-  const { username } = instagramData[0];
+// const Insta = () => {
+//   const instagramData = useInsta({ variables: { limit: 1 } });
+//   const { username } = instagramData[0];
 
-  return (
-    <>
-      <H2>Instagram posts! from @{username}</H2>
-      <Container>
-        <A
-          href={`https://instagram.com/${username}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ display: 'none' }}
-        >
-          hello
-        </A>
+//   return (
+//     <>
+//       <H2>Instagram posts! from @{username}</H2>
+//       <Container>
+//         <A
+//           href={`https://instagram.com/${username}`}
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           style={{ display: 'none' }}
+//         >
+//           hello
+//         </A>
 
-        {instagramData.map(d => (
-          <A key={d.id} href={`https://instagram.com/p/${d.id}/`}>
-            <Image fluid={d.fluid}></Image>
-          </A>
-        ))}
-      </Container>
-    </>
-  );
-};
+//         {instagramData.map(d => (
+//           <A key={d.id} href={`https://instagram.com/p/${d.id}/`}>
+//             <Image fluid={d.fluid}></Image>
+//           </A>
+//         ))}
+//       </Container>
+//     </>
+//   );
+// };
 
-export default Insta;
+// export default Insta;
